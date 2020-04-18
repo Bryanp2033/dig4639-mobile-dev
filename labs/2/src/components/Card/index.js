02319d8ec42305c3ad3243e58a1a7319479a4a08
Bryan Porras <bryanporras@Bryans-MacBook-Pro-2.local>
2020-04-18 13:20:16 -0400

    redid lab 2

---
+++ labs/2/src/components/Card/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+class Card {
+    constructor(props) {
+        // Stores the argument in this.props
+        this.props = props;
+}
+    render() {
+        return this.element;
+    }
+}
+
+export default Card;
--- labs/2/src/index.js
+++ labs/2/src/index.js
@@ -1,12 +1,13 @@
-let Card; // TODO: Replace this with the correct import statement
+import Card from "./components/Card/index.js";
+
 function runOnLoad()
 {
-        // Create a container for us
-            let element = document.createElement("div");
-            element.id = "container";
-            document.body.appendChild(element);
-            var newCard = new Card({content:"Sample value provided"});
-            element.appendChild(newCard.render());
-        }
-        
-        window.addEventListener("DOMContentLoaded", runOnLoad);
+    // Create a container for us
+    let element = document.createElement("div");
+    element.id = "container";
+    document.body.appendChild(element);
+    var newCard = new Card({content:"Sample value provided"});
+    element.appendChild(newCard.render());
+}
+
+window.addEventListener("DOMContentLoaded", runOnLoad);
